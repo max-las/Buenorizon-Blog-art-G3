475ef3b8b7607d8a96da12a3ea4c8aaef3542dc9
The-SamL <samuel.labagnere@mmibordeaux.com>
2021-02-09 10:13:04 +0100

    ADD article.class.php

---
+++ CLASS_CRUD/article.class.php
@@ -0,0 +1,129 @@
+<?php
+	// CRUD ANGLE (ETUD)
+
+	require_once __DIR__ . '../../CONNECT/database.php';
+
+	class ANGLE{
+		function get_1Article($numArt){
+			global $db;
+
+			$query = 'SELECT * FROM angle WHERE numArt = ?';
+
+			$request = $db->prepare($query);
+	
+			$request->execute(array($numArt));
+	
+			$result = $request->fetch();
+	
+			$request->closeCursor();
+			return ($result);
+		}
+
+		function get_AllArticles(){
+			global $db;
+
+			$query = 'SELECT * FROM article';
+
+			$request = $db->query($query);
+		
+			$result = $request->fetchAll();
+	
+			$request->closeCursor();
+			return ($result);
+		}
+
+		function get_AllArticlesByAngle($numAngl){
+			global $db;
+
+			$query = 'SELECT * FROM article WHERE numAngl = ?';
+
+			$request = $db->prepare($query);
+	
+			$request->execute(array($numAngl));
+	
+			$result = $request->fetchAll();
+	
+			$request->closeCursor();
+			return ($result);
+        }
+        
+        function get_AllArticlesByThematique($numThem){
+			global $db;
+
+			$query = 'SELECT * FROM article WHERE numThem = ?';
+
+			$request = $db->prepare($query);
+	
+			$request->execute(array($numThem));
+	
+			$result = $request->fetchAll();
+	
+			$request->closeCursor();
+			return ($result);
+		}
+
+		function create($dtCreArt, $libTitrArt, $libChapoArt, $libAccrochArt, $parag1Art, $libSsTitr1Art, $parag2Art, $libSsTitrArt, $parag3Art, $libConclArt, $urlPhotArt, $numAngl, $numThem){
+			global $db;
+			try {
+				$query = "INSERT INTO article (dtCreArt, libTitrArt, libChapoArt, libAccrochArt, parag1Art, libSsTitr1Art, parag2Art, libSsTitr2Art, parag3Art, libConclArt, urlPhotArt, numAngl, numThem) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+				$db->beginTransaction();
+
+				$request = $db->prepare($query);
+				
+				$request->execute(array($dtCreArt, $libTitrArt, $libChapoArt, $libAccrochArt, $parag1Art, $libSsTitr1Art, $parag2Art, $libSsTitrArt, $parag3Art, $libConclArt, $urlPhotArt, $numAngl, $numThem));
+
+				$db->commit();
+
+				$request->closeCursor();
+			}
+			catch (PDOException $e) {
+					die('Erreur insert ARTICLE : ' . $e->getMessage());
+					$db->rollBack();
+					$request->closeCursor();
+			}
+		}
+
+		function update($numArt, $dtCreArt, $libTitrArt, $libChapoArt, $libAccrochArt, $parag1Art, $libSsTitr1Art, $parag2Art, $libSsTitrArt, $parag3Art, $libConclArt, $urlPhotArt, $numAngl, $numThem){
+			global $db;
+			try {
+				$db->beginTransaction();
+
+				$query = "UPDATE article SET dtCreArt = ?, libTitrArt = ?, libChapoArt = ?, libAccrochArt = ?, parag1Art = ?, libSsTitr1Art = ?, parag2Art = ?, libSsTitr2Art = ?, parag3Art = ?, libConclArt = ?, urlPhotArt = ?, numAngl = ?, numThem = ? WHERE numArt = ?";
+
+				$request = $db->prepare($query);
+
+				$request->execute(array($numArt, $dtCreArt, $libTitrArt, $libChapoArt, $libAccrochArt, $parag1Art, $libSsTitr1Art, $parag2Art, $libSsTitrArt, $parag3Art, $libConclArt, $urlPhotArt, $numAngl, $numThem));
+
+				$db->commit();
+				$request->closeCursor();
+			}
+			catch (PDOException $e) {
+					die('Erreur update ARTICLE : ' . $e->getMessage());
+					$db->rollBack();
+					$request->closeCursor();
+			}
+		}
+
+		function delete($numArt){
+			global $db;
+			try {
+				$query = "DELETE FROM article WHERE numArt = ?";
+
+          		$db->beginTransaction();
+
+				$request = $db->prepare($query);
+
+				$request->execute(array($numArt));
+
+				$db->commit();
+				$request->closeCursor();
+
+			}
+			catch (PDOException $e) {
+					die('Erreur delete ARTICLE : ' . $e->getMessage());
+					$db->rollBack();
+					$request->closeCursor();
+			}
+		}
+	}	// End of class
