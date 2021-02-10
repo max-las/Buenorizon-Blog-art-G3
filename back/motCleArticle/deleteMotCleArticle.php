5a3416b3bcfc27472faff6c8999b1b5c44755f6c
The-SamL <samuel.labagnere@mmibordeaux.com>
2021-02-10 11:49:45 +0100

    MotCleArticle.php complete edition

--- back/motCleArticle/deleteMotCleArticle.php
+++ back/motCleArticle/deleteMotCleArticle.php
@@ -5,46 +5,42 @@ require_once __DIR__ . '/../../util/utilErrOn.php';
 
 
     // Init variables form
-    include __DIR__ . '/initLikeCom.php';
+    include __DIR__ . '/initMotCleArticle.php';
     $deleted = false;
-    if(!isset($_GET['numMemb'])) $_GET['numMemb'] = '';
     if(!isset($_GET['numArt'])) $_GET['numArt'] = '';
+    if(!isset($_GET['numMotCle'])) $_GET['numMotCle'] = '';
     // controle des saisies du formulaire
 
 
     // insertion classe MEMBRE
-    require_once __DIR__ . '/../../CLASS_CRUD/likeCom.class.php';
-    $class = new LIKECOM;
+    require_once __DIR__ . '/../../CLASS_CRUD/motclearticle.class.php';
+    $class = new MOTCLEARTICLE;
 
 
     // Gestion du $_SERVER["REQUEST_METHOD"] => En POST
     // suppression effective du statut
     if($_SERVER["REQUEST_METHOD"] == 'POST'){
         if($_POST["Submit"] === "Annuler"){
-            header("Location: ./likeCom.php");
+            header("Location: ./motCleArticle.php");
             die();
         }
 
-        $numMemb = $_POST["numMemb"];
-        $numSeqCom = $_POST['numSeqCom'];
-        $numArt = $_POST['numArt'];
-        $likeC = $_POST['likeC'];
+        $numArt = $_POST["numArt"];
+        $numMotCle = $_POST['numMotCle'];
         
-        $class->delete($numMemb, $numSeqCom, $numArt);
+        $class->delete($numMotCle, $numArt);
         $deleted = true;
 
     }else{
-        $numMemb = $_GET["numMemb"];
-        $numSeqCom = $_GET['numSeqCom'];
-        $numArt = $_GET['numArt'];
+        $numArt = $_GET["numArt"];
+        $numMotCle = $_GET['numMotCle'];
     }
 
-    $resultLikeCom = $class->get_1LikeCom($numMemb, $numSeqCom, $numArt);
+    $resultMotCleArticle = $class->get_1MotCleArticle($numMotCle, $numArt);
 
-    if($resultLikeCom){
-        $numMemb = $resultLikeCom['numMemb'];
-        $numArt = $resultLikeCom['numArt'];
-        $likeC = $resultLikeCom['likeC'];
+    if($resultMotCleArticle){
+        $numArt = $resultMotCleArticle['numArt'];
+        $numMotCle = $resultMotCleArticle['numMotCle'];
     }
 
 ?>
@@ -52,7 +48,7 @@ require_once __DIR__ . '/../../util/utilErrOn.php';
 <html lang="fr">
 <head>
     <meta charset="utf-8" />
-    <title>Admin - Gestion du CRUD Like Commentaire</title>
+    <title>Admin - Gestion du CRUD MotCleArticle</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="" />
     <meta name="author" content="" />
@@ -85,7 +81,7 @@ require_once __DIR__ . '/../../util/utilErrOn.php';
 
 <head>
     <meta charset="utf-8" />
-    <title>Admin - Gestion du CRUD Like Commentaire</title>
+    <title>Admin - Gestion du CRUD Mot-Clé Article</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="" />
     <meta name="author" content="" />
@@ -94,25 +90,17 @@ require_once __DIR__ . '/../../util/utilErrOn.php';
 </head>
 
 <body class="ui container">
-    <h1>BLOGART21 Admin - Gestion du CRUD Like Article</h1>
-    <h2>Modification d'un Like Article</h2>
+    <h1>BLOGART21 Admin - Gestion du CRUD Mot-Clé Article</h1>
+    <h2>Modification d'un Mot-Clé Article</h2>
     <br>
-    <form method="post" action=".\deleteLikeCom.php" class="ui form">
-        <div class="field">
-            <label>Numéro du Membre</label>
-            <input type="number" name="numMemb" placeholder="N° Membre" value="<? echo $numMemb ?>" readonly>
-        </div>
-        <div class="field">
-            <label>Numéro du Commentaire</label>
-            <input type="number" name="numSeqCom" placeholder="N° Commentaire" value="<? echo $numSeqCom ?>" readonly>
-        </div>
+    <form method="post" action=".\deleteMotCleArticle.php" class="ui form">
         <div class="field">
             <label>Numéro de l'Article</label>
             <input type="number" name="numArt" placeholder="N° Article" value="<? echo $numArt ?>" readonly>
         </div>
         <div class="field">
-            <label>Like</label>
-            <input type="number" min=0 max=1 name="likeC" placeholder="Like" value="<? echo $likeC ?>" readonly>
+            <label>Numéro du Mot-Clé</label>
+            <input type="number" name="numMotCle" placeholder="N° Mot-Clé" value="<? echo $numMotCle ?>" readonly>
         </div>
         <br>
         <input class="ui button" type="submit" name="Submit" value="Annuler">
@@ -122,10 +110,10 @@ require_once __DIR__ . '/../../util/utilErrOn.php';
 <?php
 
 if($deleted) {
-    echo '<p style="color:green;">Le like commentaire ' . $numMemb . '#' . $numSeqCom . ' #' . $numArt . ' a été supprimé.</p>';
+    echo '<p style="color:green;">Le mot-clé article ' . $numArt . ' #' . $numMotCle . ' a été supprimé.</p>';
 }
 
-require_once __DIR__ . '/footerLikeCom.php';
+require_once __DIR__ . '/footerMotCleArticle.php';
 
 require_once __DIR__ . '/footer.php';
 ?>
--- back/motCleArticle/motCleArticle.php
+++ back/motCleArticle/motCleArticle.php
@@ -1,38 +1,59 @@
 <?php
-/////////////////////////////////////////////////////
-//
-//  CRUD MOTCLEARTICLE (PDO) - Modifié - 6 Décembre 2020
-//
-//  Script  : motCleArticle.php  (ETUD)   -   BLOGART21
-//
-/////////////////////////////////////////////////////
-
-// Mode DEV
 require_once __DIR__ . '/../../util/utilErrOn.php';
-
+require_once __DIR__ . '/../../CLASS_CRUD/motclearticle.class.php';
+$class = new MOTCLEARTICLE;
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="utf-8" />
-    <title>Admin - Gestion du CRUD Mots-Clés / Article</title>
+    <title>Admin - Gestion du CRUD MotCleArticle</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="" />
     <meta name="author" content="" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
 
-    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+    <!-- <link href="../css/style.css" rel="stylesheet" type="text/css" /> -->
 </head>
+
 <body>
-    <h1>BLOGART21 Admin - Gestion du CRUD Mots-Clés / Article</h1>
+    <div class="ui container">
+        <h1>BLOGART21 Admin - Gestion du CRUD Mot-Clé Article</h1>
+        <hr>
+        <h2><a href="./createMotCleArticle.php">Nouveau Mot-Clé Article</a></h2>
+        <hr>
+        <h2>Tous les Mots-clés Articles</h2>
+        <table class="ui celled table">
+            <thead>
+                <tr>
+                    <th>Numéro Article</th>
+                    <th>Numéro Mot-Clé</th>
+                    <th colspan="2">&nbsp;Action&nbsp;</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $allMotCle = $class->get_AllMotCle();
+                foreach ($allMotCle as $row) { ?>
+                    <tr>
+                        <td><?= $row['numArt'] ?></td>
+                        <td><?= $row['numMotCle'] ?></td>
+                        <td>&nbsp;<a href="./updateMotCleArticle.php?numArt=<?= $row['numArt']; ?>&numMotCle=<?= $row['numMotCle']; ?>"><i>Modifier</i></a>&nbsp;
+                        <br />
+                    </td>
+                    <td>&nbsp;<a href="./deleteMotCleArticle.php?numArt=<?= $row['numArt']; ?>&numMotCle=<?= $row['numMotCle']; ?>"><i>Supprimer</i></a>&nbsp;
+                        <br />
+                    </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
 
-    <br><br>
-
-    <h2>En construction :-)</h2>
-
-    <br><br>
-
-<?php
-require_once __DIR__ . '/footer.php';
-?>
+        <?php
+        require_once __DIR__ . '/footerMotCleArticle.php';
+        require_once __DIR__ . '/footer.php';
+        ?>
+    </div>
 </body>
+
 </html>
