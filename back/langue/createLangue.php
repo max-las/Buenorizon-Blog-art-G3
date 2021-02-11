9c4a79b6ac3dec51da1d83ca9dfc78bc62c35a5e
The-SamL <samuel.labagnere@mmibordeaux.com>
2021-02-11 11:30:52 +0100

    Redesign CRUD Langue

--- back/langue/createLangue.php
+++ back/langue/createLangue.php
@@ -49,27 +49,24 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
 
-    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+    <!-- <link href="../css/style.css" rel="stylesheet" type="text/css" /> -->
 </head>
-<body>
+<body class="ui container">
     <h1>BLOGART21 Admin - Gestion du CRUD Langue</h1>
-    <h2>Ajout d'une langue</h2>
-
-    <form method="post" action="./createLangue.php" enctype="multipart/form-data">
-
-      <fieldset>
-        <legend class="legend1">Formulaire Langue...</legend>
-
-        <!-- <input type="hidden" id="id" name="id" value="<?= $_GET['id']; ?>" /> -->
-
-        <div class="control-group">
-            <label class="control-label" for="lib1Lang"><b>Désignation :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
-            <input type="text" name="lib1Lang" id="lib1Lang" size="80" maxlength="80" autofocus/><br><br>
-
-            <label class="control-label" for="lib2Lang"><b>Dénomination :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
-            <input type="text" name="lib2Lang" id="lib2Lang" size="80" maxlength="80"/><br><br>
-
-            <label class="control-label" for="numPays"><b>Pays :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
+    <h2>Suppression d'une Langue</h2>
+    <br>
+    <form method="post" action=".\createLangue.php" class="ui form">
+        <div class="field">
+            <label>Désignation</label>
+            <input type="text" name="lib1Lang" id="lib1Lang" placeholder="Désignation">
+        </div>
+        <div class="field">
+            <label>Dénomination</label>
+            <input type="text" name="lib2Lang" id="lib2Lang" placeholder="Dénomination">
+        </div>
+        <div class="field">
+            <label>Pays</label>
             <select name="numPays" id="numPays"> 
             <?php
                 $allPays = $monPays->get_AllPays();
@@ -79,18 +76,8 @@
             ?>
             </select>
         </div>
-
-        <div class="control-group">
-            <div class="controls">
-                <br><br>
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <input type="submit" value="Initialiser" style="cursor:pointer; padding:5px 20px; background-color:lightsteelblue; border:dotted 2px grey; border-radius:5px;" name="Submit" />
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <input type="submit" value="Valider" style="cursor:pointer; padding:5px 20px; background-color:lightsteelblue; border:dotted 2px grey; border-radius:5px;" name="Submit" />
-                <br>
-            </div>
-        </div>
-      </fieldset>
+        <br>
+        <input class="ui button" type="submit" name="Submit" value="Valider">
     </form>
 <?php
 
--- back/langue/langue.php
+++ back/langue/langue.php
@@ -26,64 +26,45 @@ $maLangue = new LANGUE;
     <meta name="description" content="" />
     <meta name="author" content="" />
 
-    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+    <!--<link href="../css/style.css" rel="stylesheet" type="text/css" />-->
 </head>
 <body>
-    <h1>BLOGART21 Admin - Gestion du CRUD Langue</h1>
 
-    <hr /><br />
-    <h2>Nouvelle langue :&nbsp;<a href="./createLangue.php"><i>Créer une langue</i></a></h2>
-    <br />
-    <hr />
-    <h2>Toutes les langues</h2>
-
-    <table border="3" bgcolor="aliceblue">
-        <thead>
-            <tr>
-                <th>&nbsp;Numéro&nbsp;</th>
-                <th>&nbsp;Désignation&nbsp;</th>
-                <th>&nbsp;Dénomination&nbsp;</th>
-                <th>&nbsp;Pays&nbsp;</th>
-                <th colspan="2">&nbsp;Action&nbsp;</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-
-            $allLangues = $maLangue->get_AllLanguesByPays();
-            foreach ($allLangues as $row) {
-
-                // Appel méthode : tous les statuts en BDD
-
-                // Boucle pour afficher
-                //foreach($all as $row) {
-            ?>
+    <div class="ui container">
+        <h1>BLOGART21 Admin - Gestion du CRUD Langue</h1>
+        <hr>
+        <h2><a href="./createLangue.php">Nouvelle Langue</a></h2>
+        <hr>
+        <h2>Toutes les Langues</h2>
+        <table class="ui celled table">
+            <thead>
                 <tr>
-                    <td>
-                        <h4>&nbsp; <?php echo $row['numLang']; ?> &nbsp;</h4>
-                    </td>
-
-                    <td>&nbsp; <?php echo $row['lib1Lang']; ?> &nbsp;</td>
-
-                    <td>&nbsp; <?php echo $row['lib2Lang']; ?> &nbsp;</td>
-
-                    <td>&nbsp; <?php echo $row['frPays']; ?> &nbsp;</td>
-
-                    <td>&nbsp;<a href="./updateLangue.php?id=<?= $row['numLang'] ?>"><i>Modifier</i></a>&nbsp;
-                        <br />
-                    </td>
-                    <td>&nbsp;<a href="./deleteLangue.php?id=<?= $row['numLang'] ?>"><i>Supprimer</i></a>&nbsp;
-                        <br />
-                    </td>
+                    <th>Numéro</th>
+                    <th>Désignation</th>
+                    <th>Dénomination</th>
+                    <th>Pays</th>
+                    <th colspan="2">&nbsp;Action&nbsp;</th>
                 </tr>
-            <?php
-            }
-            //}	// End of foreach
-            ?>
-        </tbody>
-    </table>
-    <br><br>
-
+            </thead>
+            <tbody>
+                <?php $allLangues = $maLangue->get_AllLanguesByPays();
+                    foreach ($allLangues as $row) { ?>
+                    <tr>
+                        <td><?= $row['numLang'] ?></td>
+                        <td><?= $row['lib1Lang'] ?></td>
+                        <td><?= $row['lib2Lang'] ?></td>
+                        <td><?= $row['frPays'] ?></td>
+                        <td>&nbsp;<a href="./updateLangue.php?id=<?= $row['numLang'] ?>"><i>Modifier</i></a>&nbsp;
+                        <br />
+                        </td>
+                        <td>&nbsp;<a href="./deleteLangue.php?id=<?= $row['numLang'] ?>"><i>Supprimer</i></a>&nbsp;
+                        <br />
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
 <?php
 require_once __DIR__ . '/footer.php';
 ?>
