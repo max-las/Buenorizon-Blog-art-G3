0be711afb0c73c5e7ed19252613b1e06ce62372d
LeFlan <clement.renou@mmibordeaux.com>
2021-02-16 02:18:35 +0100

    Commençage style navbar

--- front/includes/commons/header.php
+++ front/includes/commons/header.php
@@ -1,10 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../../assets/css/main.css">
 </head>
+<header>
+    <div class="navbar">
+        <div>
+            <a href="../pages/home.php">Acceuil</a>
+            <a href="../pages/article.php">Articles</a>
+            <a href="../pages/contact.php">Contact</a>
+            <a href="../pages/about.php">A propos</a>
+        </div>
+        <div>
+            <a href="../pages/login.php">Connexion</a>
+            <button>S'inscrire</button>
+            <!-- <a href="../pages/signin.php">S'inscrire</a> -->
+        </div>
+    </div>
+
+</header>
+
 <body>
-aaaaaaa
