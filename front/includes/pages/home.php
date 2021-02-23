924ef917f33445f889ecd9ab7547a128e0da77a3
YaniMad <yani.madebos@mmibordeaux.com>
2021-02-23 11:21:11 +0100

    accès au push de theo

--- front/includes/pages/home.php
+++ front/includes/pages/home.php
@@ -4,6 +4,15 @@ require_once('../commons/header.php');
 
 <!-- Put your code here my friend ;) -->
 
+<div class="container-arrivee">
+<img src="./../../assets/img/" alt="">
+
+
+</div>
+
+
+
+
 <?php
 require_once('../commons/footer.php');
 ?>
