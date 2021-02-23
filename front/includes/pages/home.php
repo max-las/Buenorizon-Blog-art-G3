52558fad797d9e9a3c3f44da5d67675b096a1d48
YaniMad <yani.madebos@mmibordeaux.com>
2021-02-23 17:11:16 +0100

    oui

--- front/includes/pages/home.php
+++ front/includes/pages/home.php
@@ -4,11 +4,11 @@ require_once('../commons/header.php');
 
 <!-- Put your code here my friend ;) -->
 <div class="container-arrivee">
-    <div class="background">
-        <img src="./../../assets/img/drone-carrousel.jpg" alt="Drone_fond_carrousel">
-    </div>
     <div class="content">
         <h1>DRONISOS</h1>
+    </div>
+    <div class="background">
+        <img src="./../../assets/img/drone-carrousel.jpg" alt="Drone_fond_carrousel">
     </div>
 </div>
 
