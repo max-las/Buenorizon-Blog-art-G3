dfe324a61e7bb19143a271bb39d4b4f251dbdce2
YaniMad <yani.madebos@mmibordeaux.com>
2021-02-23 17:07:10 +0100

    debut home

--- front/includes/pages/home.php
+++ front/includes/pages/home.php
@@ -3,14 +3,13 @@ require_once('../commons/header.php');
 ?>
 
 <!-- Put your code here my friend ;) -->
-
 <div class="container-arrivee">
-<img src="./../../assets/img/drone-carrousel.jpg" alt="Drone_fond_carrousel">
-
-<h1>DRONISOS</h1>
-
-
-
+    <div class="background">
+        <img src="./../../assets/img/drone-carrousel.jpg" alt="Drone_fond_carrousel">
+    </div>
+    <div class="content">
+        <h1>DRONISOS</h1>
+    </div>
 </div>
 
 
