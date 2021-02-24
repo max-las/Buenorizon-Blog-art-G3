25f8dbe1c553e7e4b919d0fde8590ad2e62229da
LeFlan <clement.renou@mmibordeaux.com>
2021-02-24 12:31:48 +0100

    search bar finition

--- front/includes/pages/article.php
+++ front/includes/pages/article.php
@@ -1,10 +1,10 @@
 <div>
     <div class="search">
         <input type="input" placeholder="Rechercher un article...">
-        <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
             <circle cx="10.5" cy="10.5" r="7.5"></circle>
             <line x1="21" y1="21" x2="15.8" y2="15.8"></line>
         </svg>
-        <label>Rechercher un article ...</label>
+        <label>Rechercher un article</label>
     </div>
 </div>
