a6fe7071e0810d712c83e4fd53805bb6427e16bc
0ur0boss <enzoduret@yahoo.fr>
2021-02-24 12:41:02 +0100

    pull

--- front/includes/pages/login.php
+++ front/includes/pages/login.php
@@ -4,6 +4,50 @@ require_once('../commons/header.php');
 
 <!-- Put your code here my friend ;) -->
 
+
+<div class="container-global">
+
+    <h1>Connexion</h1>
+
+    <div class="ice0">
+        <svg width="423" height="511" viewBox="0 0 423 511" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <g filter="url(#filter0_b)">
+                <rect width="423" height="511" fill="url(#paint0_linear)" />
+            </g>
+            <g style="mix-blend-mode:overlay" opacity="0.15">
+                <path d="M285.301 294.158C312.089 226.123 376.53 193.774 422.998 185.92V510.634H47.1504C115.373 466.823 258.514 362.193 285.301 294.158Z" fill="white" />
+            </g>
+            <g style="mix-blend-mode:overlay" opacity="0.05">
+                <rect width="423" height="511" fill="url(#pattern0)" />
+            </g>
+            <defs>
+                <filter id="filter0_b" x="-100" y="-100" width="623" height="711" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feGaussianBlur in="BackgroundImage" stdDeviation="50" />
+                    <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur" result="shape" />
+                </filter>
+                <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1.44444" height="1.39335">
+                    <use xlink:href="#image0" transform="scale(0.00236407 0.00195695)" />
+                </pattern>
+                <linearGradient id="paint0_linear" x1="0" y1="0" x2="391.732" y2="452.518" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#12475E" stop-opacity="0.15" />
+                    <stop offset="1" stop-color="#12475E" stop-opacity="0.05" />
+                </linearGradient>
+            </defs>
+        </svg>
+
+
+
+        <div class="lign "></div>
+        <div class="lign"></div>
+
+
+        <p class=souvenir>Se souvenir de moi</p>
+
+    </div>
+
+</div>
 <?php
 require_once('../commons/footer.php');
 ?>
