144ae44f1b426dda36ceddbf1f86d1393eba9d8f
0ur0boss <enzoduret@yahoo.fr>
2021-02-25 09:40:48 +0100

    pull

--- front/includes/pages/signin.php
+++ front/includes/pages/signin.php
@@ -5,107 +5,107 @@ require_once('../commons/header.php');
 <!-- Put your code here my friend ;) -->
 
 
-    <div class="container-signin">
+<div class="container-signin">
 
-        <h1>Enregistrez vous</h1>
-        <p class="soustitre">Les champs marqués d’un <span class=red>*</span> sont obligatoires.</p>
+    <h1>Enregistrez vous</h1>
+    <p class="soustitre">Les champs marqués d’un <b class=red>*</b> sont obligatoires.</p>
 
-        <div class="signin">
-            <div class="background1">
-                <svg width="416" height="670" viewBox="0 0 416 670" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <g filter="url(#filter0_b)">
-                        <rect width="416" height="670" fill="url(#paint0_linear)" />
-                    </g>
-                    <g style="mix-blend-mode:overlay" opacity="0.15">
-                        <path d="M187.991 291.593C132.191 184.413 34.3444 100.812 0.00095502 79.4048L0 669.998H162.684C229.737 593.451 251.256 413.113 187.991 291.593Z" fill="white" />
-                    </g>
-                    <g style="mix-blend-mode:overlay" opacity="0.05">
-                        <rect width="415.09" height="670" fill="url(#pattern0)" />
-                    </g>
-                    <defs>
-                        <filter id="filter0_b" x="-100" y="-100" width="616" height="870" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                            <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                            <feGaussianBlur in="BackgroundImage" stdDeviation="50" />
-                            <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur" />
-                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur" result="shape" />
-                        </filter>
-                        <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1.47197" height="1.06269">
-                            <use xlink:href="#image0" transform="scale(0.00240912 0.00149254)" />
-                        </pattern>
-                        <linearGradient id="paint0_linear" x1="0" y1="0" x2="513.688" y2="445.087" gradientUnits="userSpaceOnUse">
-                            <stop stop-color="#12475E" stop-opacity="0.15" />
-                            <stop offset="1" stop-color="#12475E" stop-opacity="0.05" />
-                        </linearGradient>
-                    </defs>
-                </svg>
+    <div class="signin">
+        <div class="background1">
+            <svg width="416" height="670" viewBox="0 0 416 670" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <g filter="url(#filter0_b)">
+                    <rect width="416" height="670" fill="url(#paint0_linear)" />
+                </g>
+                <g style="mix-blend-mode:overlay" opacity="0.15">
+                    <path d="M187.991 291.593C132.191 184.413 34.3444 100.812 0.00095502 79.4048L0 669.998H162.684C229.737 593.451 251.256 413.113 187.991 291.593Z" fill="white" />
+                </g>
+                <g style="mix-blend-mode:overlay" opacity="0.05">
+                    <rect width="415.09" height="670" fill="url(#pattern0)" />
+                </g>
+                <defs>
+                    <filter id="filter0_b" x="-100" y="-100" width="616" height="870" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <feGaussianBlur in="BackgroundImage" stdDeviation="50" />
+                        <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur" />
+                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur" result="shape" />
+                    </filter>
+                    <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1.47197" height="1.06269">
+                        <use xlink:href="#image0" transform="scale(0.00240912 0.00149254)" />
+                    </pattern>
+                    <linearGradient id="paint0_linear" x1="0" y1="0" x2="513.688" y2="445.087" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#12475E" stop-opacity="0.15" />
+                        <stop offset="1" stop-color="#12475E" stop-opacity="0.05" />
+                    </linearGradient>
+                </defs>
+            </svg>
 
-            </div>
-            <div class="content1">
-                <form class="form-inscription" action="" method="post">
+        </div>
+        <div class="content1">
+            <form class="form-inscription" action="" method="post">
 
-                    <div class="form-group">
-                        <input type="input" placeholder="Rechercher un article...">
-                        <label>Pseudo *<label>
-                    </div>
+                <div class="form-group">
+                    <input type="input" placeholder="Rechercher un article...">
+                    <label>Pseudo <b class=red>*</b><label>
+                </div>
 
-                    <div class="form-group">
-                        <input type="input" placeholder="Rechercher un article...">
-                        <label>Prénom<label>
-                    </div>
+                <div class="form-group">
+                    <input type="input" placeholder="Rechercher un article...">
+                    <label>Prénom<label>
+                </div>
 
-                    <div class="form-group">
-                        <input type="input" placeholder="Rechercher un article...">
-                        <label>Nom<label>
-                    </div>
+                <div class="form-group">
+                    <input type="input" placeholder="Rechercher un article...">
+                    <label>Nom<label>
+                </div>
 
-                    <div class="form-group">
-                        <input type="input" placeholder="Rechercher un article...">
-                        <label>Adresse mail *<label>
-                    </div>
-
-
+                <div class="form-group">
+                    <input type="input" placeholder="Rechercher un article...">
+                    <label>Adresse mail <b class=red>*</b><label>
+                </div>
 
 
-                    <div class="form-group">
-                        <input id="input-signin" type="password" placeholder="Rechercher un article...">
-                        <svg id="eye" class="eye" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye">
-                            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                            <circle cx="12" cy="12" r="3"></circle>
-                        </svg>
-                        <label>Mot de passe *<label>
-                    </div>
-
-                    <div class="form-group">
-                        <input id="input-signin" type="password" placeholder="Rechercher un article...">
-                        <svg id="eye" class="eye" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye">
-                            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                            <circle cx="12" cy="12" r="3"></circle>
-                        </svg>
-                        <label>Confirmer le mot de passe *<label>
-                    </div>
 
 
-                    <div class="souvenir">
-                        <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M6.40039 8.11131L9.10033 10.778L19.0001 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            <path d="M17.1997 9.00071V15.2231C17.1997 15.6946 17.01 16.1468 16.6725 16.4802C16.3349 16.8136 15.8771 17.0009 15.3997 17.0009H2.79996C2.32258 17.0009 1.86476 16.8136 1.5272 16.4802C1.18964 16.1468 1 15.6946 1 15.2231V2.77832C1 2.30681 1.18964 1.85461 1.5272 1.5212C1.86476 1.18779 2.32258 1.00049 2.79996 1.00049H12.6998" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                        <span>Se souvenir de moi</span>
-                    </div>
+                <div class="form-group">
+                    <input id="input-signin" type="password" placeholder="Rechercher un article...">
+                    <svg id="eye" class="eye" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye">
+                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                        <circle cx="12" cy="12" r="3"></circle>
+                    </svg>
+                    <label>Mot de passe <b class=red>*</b><label>
+                </div>
 
-                    <div class="cdu">
-                        <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M6.40039 8.11131L9.10033 10.778L19.0001 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            <path d="M17.1997 9.00071V15.2231C17.1997 15.6946 17.01 16.1468 16.6725 16.4802C16.3349 16.8136 15.8771 17.0009 15.3997 17.0009H2.79996C2.32258 17.0009 1.86476 16.8136 1.5272 16.4802C1.18964 16.1468 1 15.6946 1 15.2231V2.77832C1 2.30681 1.18964 1.85461 1.5272 1.5212C1.86476 1.18779 2.32258 1.00049 2.79996 1.00049H12.6998" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                        <span>Accepter les <a href="front\includes\pages\about.php" style="color:#FFFFFF; " Vers nos Conditions générales">Conditions générales d’utilisation</a></span>
-                    </div>
+                <div class="form-group">
+                    <input id="input-signin" type="password" placeholder="Rechercher un article...">
+                    <svg id="eye" class="eye" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye">
+                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                        <circle cx="12" cy="12" r="3"></circle>
+                    </svg>
+                    <label>Confirmer le mot de passe <b class=red>*</b><label>
+                </div>
 
-                    <button>S'enregistrer</button>
-                </form>
-            </div>
+
+                <div class="souvenir">
+                    <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6.40039 8.11131L9.10033 10.778L19.0001 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M17.1997 9.00071V15.2231C17.1997 15.6946 17.01 16.1468 16.6725 16.4802C16.3349 16.8136 15.8771 17.0009 15.3997 17.0009H2.79996C2.32258 17.0009 1.86476 16.8136 1.5272 16.4802C1.18964 16.1468 1 15.6946 1 15.2231V2.77832C1 2.30681 1.18964 1.85461 1.5272 1.5212C1.86476 1.18779 2.32258 1.00049 2.79996 1.00049H12.6998" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <span>Se souvenir de moi</span>
+                </div>
+
+                <div class="cdu">
+                    <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6.40039 8.11131L9.10033 10.778L19.0001 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M17.1997 9.00071V15.2231C17.1997 15.6946 17.01 16.1468 16.6725 16.4802C16.3349 16.8136 15.8771 17.0009 15.3997 17.0009H2.79996C2.32258 17.0009 1.86476 16.8136 1.5272 16.4802C1.18964 16.1468 1 15.6946 1 15.2231V2.77832C1 2.30681 1.18964 1.85461 1.5272 1.5212C1.86476 1.18779 2.32258 1.00049 2.79996 1.00049H12.6998" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <span>Accepter les <a href="front\includes\pages\about.php" style="color:#FFFFFF; " Vers nos Conditions générales">Conditions générales d’utilisation</a></span>
+                </div>
+
+                <button>S'enregistrer</button>
+            </form>
         </div>
     </div>
+</div>
 
 
 
@@ -118,7 +118,8 @@ require_once('../commons/header.php');
 
 
 
-    <div class="visualprofil">
+<div class="visualprofil">
+    <div class="profil">
         <div class="background2">
             <svg width="360" height="425" viewBox="0 0 360 425" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <g filter="url(#filter0_b)">
@@ -162,6 +163,7 @@ require_once('../commons/header.php');
             </form>
         </div>
     </div>
+</div>
 
 
 
@@ -172,8 +174,8 @@ require_once('../commons/header.php');
 
 
 
-
-    <div class="info">
+<div class="info">
+    <div class="cguinfo ">
         <div class="background3">
             <svg width="935" height="438" viewBox="0 0 935 438" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <g filter="url(#filter0_b)">
@@ -233,6 +235,7 @@ require_once('../commons/header.php');
             </form>
         </div>
     </div>
+</div>
 
 
 
