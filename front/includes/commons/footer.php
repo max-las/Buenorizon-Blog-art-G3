3efed69994afc8c9a2254cfe4d91673bf4aa0435
0ur0boss <enzoduret@yahoo.fr>
2021-02-25 12:35:34 +0100

    footer

--- front/includes/commons/footer.php
+++ front/includes/commons/footer.php
@@ -1,5 +1,27 @@
 </main>
-<footer></footer>
+<footer class="footer">
+  <div class="container pt-5">
+    <div class="row py-5">
+      <div class="col-md-4" id="copyright">
+        <a href="/">© Eclatech 2021</a>
+      </div>
+      <div class="col-6 col-md-3 offset-md-2">
+        <ul class="list-unstyled">
+          <li class="mb-3"><a href="/about">A Propos</a></li>
+          <li class="mb-3"><a href="/contact">Contact</a></li>
+          <li class="mb-3"><a href="/cgu">Mentions légales</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md-3">
+        <ul class="list-unstyled">
+          <li class="mb-3"><a href="https://www.facebook.com" target="_blank">Facebook</a></li>
+          <li class="mb-3"><a href="https://www.instagram.com" target="_blank">Instagram</a></li>
+          <li class="mb-3"><a href="https://twitter.com" target="_blank">Twitter</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
 </body>
 
 </html>
