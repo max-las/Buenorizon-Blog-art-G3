fbe570b49a95658c3fa4d7fb9c787a002fac0ac9
The-SamL <samuel.labagnere@mmibordeaux.com>
2021-02-26 16:50:44 +0100

    correction lien back

--- index1.php
+++ index1.php
@@ -50,40 +50,40 @@ require_once __DIR__ . '/util/utilErrOn.php';
 	<br /><hr />
 	<div>
 	Gestion du CRUD :
-	<a href="./BACK/angle/angle.php">Angle </a>
+	<a href="./back/angle/angle.php">Angle </a>
 	<br /><br />
 	Gestion du CRUD :
-	<a href="./BACK/article/article.php">Article </a>
+	<a href="./back/article/article.php">Article </a>
 	<br /><br />
 	Gestion du CRUD :
-	<a href="./BACK/comment/comment.php">Commentaire </a>
+	<a href="./back/comment/comment.php">Commentaire </a>
 	<br /><br />
 	Gestion du CRUD :
-	<a href="./BACK/commentplus/commentplus.php">Réponse sur Commentaire </a>
+	<a href="./back/commentplus/commentplus.php">Réponse sur Commentaire </a>
 	<br /><br />
 	Gestion du CRUD :
-	<a href="./BACK/langue/langue.php">Langue </a>
+	<a href="./back/langue/langue.php">Langue </a>
 	<br /><br />
 	Gestion du CRUD :
-	<a href="./BACK/likeart/likeart.php">Like Article </a>
+	<a href="./back/likeart/likeart.php">Like Article </a>
 	<br /><br />
 	Gestion du CRUD :
-	<a href="./BACK/likecom/likecom.php">Like Commentaire </a>
+	<a href="./back/likecom/likecom.php">Like Commentaire </a>
 	<br /><br />
 	Gestion du CRUD :
-	<a href="./BACK/membre/membre.php">Membre </a>
+	<a href="./back/membre/membre.php">Membre </a>
 	<br /><br />
 	Gestion du CRUD :
-	<a href="./BACK/motcle/motcle.php">Mot-clé </a>
+	<a href="./back/motcle/motcle.php">Mot-clé </a>
 	<br /><br />
 	Gestion du CRUD :
-	<a href="./BACK/statut/statut.php">Statut </a>
+	<a href="./back/statut/statut.php">Statut </a>
 	<br /><br />
 	Gestion du CRUD :
-	<a href="./BACK/thematique/thematique.php">Thématique </a>
+	<a href="./back/thematique/thematique.php">Thématique </a>
 	<br /><br />
 	Gestion du CRUD :
-	<a href="./BACK/user/user.php">User </a>
+	<a href="./back/user/user.php">User </a>
 	</div>
 	<br>
 	<hr>
