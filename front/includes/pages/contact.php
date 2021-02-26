9ecfafa084fd3e3c4beda90833bc2f94bb208114
0ur0boss <enzoduret@yahoo.fr>
2021-02-26 16:43:52 +0100

    contactenfin1

--- front/includes/pages/contact.php
+++ front/includes/pages/contact.php
@@ -8,32 +8,36 @@ require_once('../commons/header.php');
 <div class="contactblock">
 
     <div class="container-contact">
-    <div>
-        <h1>Nous Contacter</h1>
-    </div>
+        <div>
+            <h1>Nous Contacter</h1>
+        </div>
         <div class="block">
             <div class="content">
 
                 <form class="form-connexion" action="./login.php" method="post">
-                <div class="form-group">
-                    <input type="input" id="" name="" placeholder="label">
-                    <label>Nom<label>
-                </div>
-                <div class="form-group">
-                    <input type="input" id="" name="" placeholder="label">
-                    <label>Adresse mail<label>
-                </div>
-                <div class="form-group">
-                    <input type="input" id="" name="" placeholder="label">
-                    <label>Votre message<label>
-                </div>
+                    <div class="infoutilisateur">
+                        <div class="form-group">
+                            <input type="input" id="" name="" placeholder="label">
+                            <label>Nom<label>
+                        </div>
+                        <div class="form-group mail">
+                            <input type="input" id="" name="" placeholder="label">
+                            <label>Adresse mail<label>
+                        </div>
+                    </div>
+                    <div class="infomessage">
+                        <div class="form-group">
+                            <input type="input" id="" name="" placeholder="label">
+                            <label>Votre message<label>
+                        </div>
+                    </div>
 
-                <button>Envoyer</button>
-            </form>
-                </div>
+                    <button class="btnenvoyer">Envoyer</button>
+                </form>
             </div>
         </div>
     </div>
+</div>
 </div>
 
 
