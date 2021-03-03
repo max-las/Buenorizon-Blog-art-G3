b2bad2700d9858e759ed943b3660c973a1ea0e54
max-las <lasserre.maxime@yahoo.fr>
2021-03-03 20:12:35 +0100

    mentions legales + cgu

--- front/includes/pages/about.php
+++ front/includes/pages/about.php
@@ -17,36 +17,11 @@ require_once('../commons/header.php');
 
         <h2 class="h2about">Mentions légales</h2 class="h2about">
         <div class="fondArticle2">
-            <p>Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des Utilisateurs du site Eclat Tech les présentes mentions légales.
-                La connexion et la navigation sur le site (indiquer le nom du site) par l’Utilisateur implique acceptation intégrale et sans réserve des présentes mentions légales.
-                Ces dernières sont accessibles sur le site à la rubrique « Mentions légales ».
-            </p>
-            <h3 class="h3about">Article 1 : L’éditeur
-            </h3 class="h3about">
-            <p>L'édition du site Eclat Tech est assurée par la Société Buenorizon créée uniquement dans le cadre du projet étudiant MMI «Blog art», adresses e-mail : contact 1 (maxime.lasserre@mmibordeaux.com), contact 2 (samuel.labagnere@mmibordeaux.com).
-            </p>
-            <h3 class="h3about"> Article 2 : L’hébergeur
-            </h3 class="h3about">
-            <p>L'hébergeur du site Eclat Tech est l’université Bordeaux Montaigne, situé à l’adresse Domaine Universitaire 33607 Pessac Cedex, avec le numéro de téléphone : 05 57 12 44 44. </p>
-            <h3 class="h3about">Article 3 : Accès au site
-            </h3 class="h3about">
-            <p>Le site est accessible par tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant d’une nécessité de maintenance.
-                En cas de modification, interruption ou suspension des services le site Eclat Tech ne saurait être tenu responsable.
-            </p>
-            <h3 class="h3about">Article 4 : Collecte des données
-            </h3 class="h3about">
-            <p>Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. Le site n’est pas déclaré à la CNIL étant créé dans le cadre d’un projet étudiant mmi Bordeaux Montaigne.
-                En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :
-            </p>
-            <h3 class="h3about">Article 5 : Propriété intellectuelle</h3 class="h3about">
-            <p>Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du site Eclat Tech, sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.
-                Pour plus d’informations, se reporter aux CGU du site Eclat Tech accessible à la rubrique « A propos\CGU » ci dessus.
-            </p>
-            <h3 class="h3about">Article 6 : Cookies</h3 class="h3about">
-            <p>L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation.
-                En naviguant sur le site, il les accepte.
-                Un cookie est un élément qui ne permet pas d’identifier l’Utilisateur mais sert à enregistrer des informations relatives à la navigation de celui-ci sur le site Internet. L’Utilisateur pourra désactiver ce cookie par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.
-            </p>
+            <p class="mlParag">L'édition du site plateforme-mmi.iut.u-bordeaux-montaigne.fr/user03/Buenorizon-Blog-art-G3 est assurée par le groupe Buenorizon constitué de Maxime Lasserre, Clément Renou, Samuel Labagnère, Enzo Duret, Yani Madebos et Théo Deschamps, et dont le siège social est situé au 1 Rue Jacques Ellul, 33800 Bordeaux<p>
+            <p class="mlParag">Adresse e-mail : enzo.duret@mmibordeaux.com</p>
+            <p class="mlParag">Numéro de téléphone : 05 57 12 20 44</p>
+            <p class="mlParag">Le Directeur de la publication est : Enzo Duret</p>
+            <p class="mlParag">L'hébergeur du site plateforme-mmi.iut.u-bordeaux-montaigne.fr/user03/Buenorizon-Blog-art-G3 est l’Université Bordeaux Montaigne, dont le siège social est situé au Domaine Universitaire, 19 esplanade des Antilles, 33607 Pessac, avec le numéro de téléphone : 05 57 12 44 44.</p>
         </div>
     </div>
 
--- front/includes/pages/cgu.php
+++ front/includes/pages/cgu.php
@@ -16,13 +16,7 @@ require_once('../commons/header.php');
     En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site.<br>
     Buenorizon se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes CGU.<br>
     </p>
-    <h3 class="h3about">Article 1 : Les mentions légales</h3>
-    <p class="cguParag">L'édition du site plateforme-mmi.iut.u-bordeaux-montaigne.fr/user03/Buenorizon-Blog-art-G3 est assurée par le groupe Buenorizon constitué de Maxime Lasserre, Clément Renou, Samuel Labagnère, Enzo Duret, Yani Madebos et Théo Deschamps, et dont le siège social est situé au 1 Rue Jacques Ellul, 33800 Bordeaux<br>
-    Adresse e-mail : enzo.duret@mmibordeaux.com.<br>
-    Le Directeur de la publication est : Enzo Duret<br>
-    L'hébergeur du site plateforme-mmi.iut.u-bordeaux-montaigne.fr/user03/Buenorizon-Blog-art-G3 est l’Université Bordeaux Montaigne, dont le siège social est situé au Domaine Universitaire, 19 esplanade des Antilles, 33607 Pessac, avec le numéro de téléphone : 05 57 12 44 44.<br>
-    </p>
-    <h3 class="h3about">ARTICLE 2 : Accès au site</h3>
+    <h3 class="h3about">ARTICLE 1 : Accès au site</h3>
     <p class="cguParag">Le site plateforme-mmi.iut.u-bordeaux-montaigne.fr/user03/Buenorizon-Blog-art-G3 permet à l'Utilisateur un accès gratuit aux services suivants :<br>
     - Lecture d'articles à caractère informatif<br>
     - Appréciation d'articles<br>
@@ -34,27 +28,27 @@ require_once('../commons/header.php');
     Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du site ou serveur et sous réserve de toute interruption ou modification en cas de maintenance, n'engage pas la responsabilité de plateforme-mmi.iut.u-bordeaux-montaigne.fr/user03/Buenorizon-Blog-art-G3. Dans ces cas, l’Utilisateur accepte ainsi ne pas tenir rigueur à l’éditeur de toute interruption ou suspension de service, même sans préavis.<br>
     L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse email de l’éditeur communiqué à l’ARTICLE 1.<br>    
     </p>
-    <h3 class="h3about">ARTICLE 3 : Collecte des données</h3>
+    <h3 class="h3about">ARTICLE 2 : Collecte des données</h3>
     <p class="cguParag">Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.<br>
     En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :<br>
     · par mail à l'adresse email enzo.duret@mmibordeaux.com<br>
-    · via un formulaire de contact ;<br>
+    · via un formulaire de contact<br>
     </p>
-    <h3 class="h3about">ARTICLE 4 : Propriété intellectuelle</h3>
+    <h3 class="h3about">ARTICLE 3 : Propriété intellectuelle</h3>
     <p class="cguParag">Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son…) font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.<br>
     L'Utilisateur doit solliciter l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus. Il s'engage à une utilisation des contenus du site dans un cadre strictement privé, toute utilisation à des fins commerciales et publicitaires est strictement interdite.<br>
     Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l’autorisation expresse de l’exploitant du site Internet constituerait une contrefaçon sanctionnée par l’article L 335-2 et suivants du Code de la propriété intellectuelle.<br>
     Il est rappelé conformément à l’article L122-5 du Code de propriété intellectuelle que l’Utilisateur qui reproduit, copie ou publie le contenu protégé doit citer l’auteur et sa source.    </p>
-    <h3 class="h3about">ARTICLE 5 : Responsabilité</h3>
+    <h3 class="h3about">ARTICLE 4 : Responsabilité</h3>
     <p class="cguParag">Les sources des informations diffusées sur le site plateforme-mmi.iut.u-bordeaux-montaigne.fr/user03/Buenorizon-Blog-art-G3 sont réputées fiables mais le site ne garantit pas qu’il soit exempt de défauts, d’erreurs ou d’omissions.<br>
     Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle. Malgré des mises à jour régulières, le site plateforme-mmi.iut.u-bordeaux-montaigne.fr/user03/Buenorizon-Blog-art-G3 ne peut être tenu responsable de la modification des dispositions administratives et juridiques survenant après la publication. De même, le site ne peut être tenue responsable de l’utilisation et de l’interprétation de l’information contenue dans ce site.<br>
     L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle que soit sa forme, est interdite. Il assume les risques liés à l'utilisation de son identifiant et mot de passe. Le site décline toute responsabilité.<br>
     Le site plateforme-mmi.iut.u-bordeaux-montaigne.fr/user03/Buenorizon-Blog-art-G3 ne peut être tenu pour responsable d’éventuels virus qui pourraient infecter l’ordinateur ou tout matériel informatique de l’Internaute, suite à une utilisation, à l’accès, ou au téléchargement provenant de ce site.<br>
     La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.</p>
-    <h3 class="h3about">ARTICLE 6 : Liens hypertextes</h3>
+    <h3 class="h3about">ARTICLE 5 : Liens hypertextes</h3>
     <p class="cguParag">Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qu’en cliquant sur ces liens, il sortira du site plateforme-mmi.iut.u-bordeaux-montaigne.fr/user03/Buenorizon-Blog-art-G3. Ce dernier n’a pas de contrôle sur les pages web sur lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de leur contenu.<br>
     </p>
-    <h3 class="h3about">ARTICLE 7 : Cookies</h3>
+    <h3 class="h3about">ARTICLE 6 : Cookies</h3>
     <p class="cguParag">L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation.<br>
     Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l’ordinateur de l’Utilisateur par votre navigateur et qui sont nécessaires à l’utilisation du site plateforme-mmi.iut.u-bordeaux-montaigne.fr/user03/Buenorizon-Blog-art-G3. Les cookies ne contiennent pas d’information personnelle et ne peuvent pas être utilisés pour identifier quelqu’un. Un cookie contient un identifiant unique, généré aléatoirement et donc anonyme. Certains cookies expirent à la fin de la visite de l’Utilisateur, d’autres restent.<br>
     L’information contenue dans les cookies est utilisée pour améliorer le site plateforme-mmi.iut.u-bordeaux-montaigne.fr/user03/Buenorizon-Blog-art-G3.<br>
@@ -63,7 +57,7 @@ require_once('../commons/header.php');
     A défaut d’acceptation, l’Utilisateur est informé que certaines fonctionnalités ou pages risquent de lui être refusées.<br>
     L’Utilisateur pourra désactiver ces cookies par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.<br>
     </p>
-    <h3 class="h3about">ARTICLE 8 : Publication par l’Utilisateur</h3>
+    <h3 class="h3about">ARTICLE 7 : Publication par l’Utilisateur</h3>
     <p class="cguParag">Le site permet aux membres de publier les contenus suivants :<br>
     Articles de blog.<br>
     Dans ses publications, le membre s’engage à respecter les règles de la Netiquette (règles de bonne conduite de l’internet) et les règles de droit en vigueur.<br>
@@ -73,7 +67,7 @@ require_once('../commons/header.php');
     Tout contenu mis en ligne par l'Utilisateur est de sa seule responsabilité. L'Utilisateur s'engage à ne pas mettre en ligne de contenus pouvant porter atteinte aux intérêts de tierces personnes. Tout recours en justice engagé par un tiers lésé contre le site sera pris en charge par l'Utilisateur.<br>
     Le contenu de l'Utilisateur peut être à tout moment et pour n'importe quelle raison supprimé ou modifié par le site, sans préavis.<br>
     </p>
-    <h3 class="h3about">ARTICLE 9 : Droit applicable et juridiction compétente</h3>
+    <h3 class="h3about">ARTICLE 8 : Droit applicable et juridiction compétente</h3>
     <p class="cguParag">La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, les tribunaux français seront seuls compétents pour en connaître.<br>
     Pour toute question relative à l’application des présentes CGU, vous pouvez joindre l’éditeur aux coordonnées inscrites à l’ARTICLE 1.<br>
     </p>
