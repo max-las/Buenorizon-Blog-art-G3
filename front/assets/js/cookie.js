632218840cf33077b4d2b61d846529daadfcdb2d
0ur0boss <enzoduret@yahoo.fr>
2021-03-03 22:40:37 +0100

    session

--- front/assets/js/cookie.js
+++ front/assets/js/cookie.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
-    if(localStorage.getItem("cookieAccepted") === null){
+    if(sessionStorage.getItem("cookieAccepted") !== null){
         $(".content_cookie").hide();
 
     }
 
     $("#btn_cookie").click(function(){
 
-        localStorage.setItem("cookieAccepted",true);
+       sessionStorage.setItem("cookieAccepted",true);
         $(".content_cookie").hide();
 
     });
